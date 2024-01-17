493f68ac680bb69967247beb9ae8318c79c81da0
Siddhanta <siddhanta.c@gmail.com>
2024-01-17 12:57:19 +0800

    Add income statement

---
+++ incomestatement.go
@@ -0,0 +1,17 @@
+package hledger
+
+import (
+	"io"
+)
+
+func (h Hledger) IncomeStatement(options Options) (io.Reader, error) {
+	rd, err := h.execCmd("incomestatement", options)
+	if err != nil {
+		e := &Error{err: err}
+		data, _ := io.ReadAll(rd)
+		e.msg = string(data)
+		return nil, e
+	}
+
+	return rd, nil
+}
