cf868a700162b8988506f1424564b061b0a993d8
Siddhanta <siddhanta.c@gmail.com>
2024-01-23 19:54:46 +0800

    Add expenses,liabilities,revenue

--- balance.go
+++ balance.go
@@ -28,3 +28,42 @@ func (h Hledger) Assets(options Options) (io.Reader, error) {
 
 	return rd, nil
 }
+
+func (h Hledger) Expenses(options Options) (io.Reader, error) {
+	options = options.WithAccountType("x")
+	rd, err := h.execCmd("balance", options)
+	if err != nil {
+		e := &Error{err: err}
+		data, _ := io.ReadAll(rd)
+		e.msg = string(data)
+		return nil, e
+	}
+
+	return rd, nil
+}
+
+func (h Hledger) Liabilities(options Options) (io.Reader, error) {
+	options = options.WithAccountType("l")
+	rd, err := h.execCmd("balance", options)
+	if err != nil {
+		e := &Error{err: err}
+		data, _ := io.ReadAll(rd)
+		e.msg = string(data)
+		return nil, e
+	}
+
+	return rd, nil
+}
+
+func (h Hledger) Revenue(options Options) (io.Reader, error) {
+	options = options.WithAccountType("r")
+	rd, err := h.execCmd("balance", options)
+	if err != nil {
+		e := &Error{err: err}
+		data, _ := io.ReadAll(rd)
+		e.msg = string(data)
+		return nil, e
+	}
+
+	return rd, nil
+}
--- balance_test.go
+++ balance_test.go
@@ -44,3 +44,24 @@ func TestAssets(t *testing.T) {
 	}
 	assert.Equal(t, expected, records)
 }
+
+// func TestExpenses(t *testing.T) {
+// 	hl := hledger.New("hledger", "data/hledger.journal")
+//
+// 	opts := hledger.NewOptions()
+// 	rd, err := hl.Expenses(opts.WithOutputCSV())
+// 	assert.NoError(t, err)
+// 	records, err := hledger.ParseCSV(rd)
+// 	assert.NoError(t, err)
+//
+// 	expected := [][]string{
+// 		{"account", "balance"},
+// 		{"assets:bank:chase", "$5348.35"},
+// 		{"assets:bank:maybank", "$-12300.00"},
+// 		{"assets:bank:stanchart", "$22.59"},
+// 		{"assets:invest:crypto", "$2000.00"},
+// 		{"assets:invest:stocks", "$1000.00"},
+// 		{"total", "$-3929.06"},
+// 	}
+// 	assert.Equal(t, expected, records)
+// }
