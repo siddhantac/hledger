dc7d08a93fc1830462dc338810d54664e929c09d
Siddhanta <siddhanta.c@gmail.com>
2024-01-27 22:48:29 +0800

    trim newline from output in case of error

--- register.go
+++ register.go
@@ -2,13 +2,14 @@ package hledger
 
 import (
 	"io"
+	"strings"
 )
 
 func (h Hledger) Register(options Options) (io.Reader, error) {
 	rd, err := h.execCmd("register", options)
 	if err != nil {
 		data, _ := io.ReadAll(rd)
-		err.msg = string(data)
+		err.msg = string(strings.Trim(string(data), "\n"))
 		return nil, err
 	}
 
